fcef96a49ecdb479cba0478070185ee0184cfa3c
wizmer <ben.coste@gmail.com>
2016-08-30 16:23:51 +0200

    A convenient way to keep configuration files within emacs

--- setup-linux-config.el
+++ setup-linux-config.el
@@ -1,50 +1,11 @@
 (defun setup-linux-config ()
   (interactive)
-  
-  (setq file-list '(
-                    
-                    ("~/.bash_aliases" . " 
-alias ip='ipython'
-alias e='emacs -nw --no-desktop'
-alias em='emacs -nw'
-alias o='xdg-open'
-alias f='find . -name'
-alias ..='cd ..'
-alias ...='cd ../..'
-alias ....='cd ../../..'
-alias .....='cd ../../../..'
-alias ......='cd ../../../../..'
-"
-                     )
 
-                    ("~/.gitconfig" . "
+  (let* ((backup-dir (expand-file-name (concat default-directory "linux-config/")))
+        (file-list (directory-files backup-dir nil "^\\([^.]\\|\\.[^.]\\|\\.\\..\\)"))
+        (home-dir "~/"))
+    (dolist (file file-list)
+        (copy-file (concat backup-dir file) (concat home-dir file) 0)
+      )
 
-\[user\]
-name = wizmer
-email = ben.coste@gmail.com
-\[alias\]
-co = checkout
-br = branch
-ci = commit
-st = status
-last = log -1 HEAD
-exists = ls-files --error-unmatch
-\[push\]
-default = simple"
-                     )
-
-                    )
-        )
-
-  (dolist (el file-list)
-    (if  (file-exists-p (car el))
-        (if (y-or-n-p (concat "overwrite " (car el) " ? (default yes) "))
-            (write-region (cdr el) nil (car el)))
-      (write-region (cdr el) nil (car el)))
-    )
-  )
-
-
-
-
-
+    ))
