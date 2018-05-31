bc34eefa9da4cc80ee1450adeeba15408bc97fa3
Benoit Coste <benoit.coste@epfl.ch>
2018-05-31 19:38:03 +0200

    Add H5 viewer mode

--- bbp.el
+++ bbp.el
@@ -14,3 +14,25 @@
 (defun neurolucida-find-duplicates ()
   (interactive)
   (re-search-forward " ( +\\(-?[0-9.]+\\) +\\(-?[0-9.]+\\) +\\(-?[0-9.]+\\).*\n *( +\\1 +\\2 +\\3"))
+
+
+(define-derived-mode h5-mode fundamental-mode "H5"
+  "major mode for editing mymath language code.")
+(add-to-list 'auto-mode-alist '("\\.h5\\'" . h5-mode))
+
+(defun h5dump ()
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (kill-buffer name)
+
+    (let ((new-buffer (get-buffer-create (generate-new-buffer-name name t))))
+      (switch-to-buffer new-buffer)
+      (shell-command
+       (concat "h5dump " filename)
+       new-buffer
+       )
+      (toggle-read-only)))
+  )
+
+(add-hook 'h5-mode-hook 'h5dump)
