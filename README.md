8844b0dabd08fc4b9422dd821fffdbdbe3b0178f
Benoît Coste <benoit.coste@epfl.ch>
2021-01-22 08:58:34 +0100

    Commit ipython config

---
+++ README.md
@@ -0,0 +1,9 @@
+# Emacs and Linux configuration
+
+## Setting up the Linux configuration
+
+```bash
+ln -s ~/.spacemacs.d/linux-config/ipython_startup ~/.ipython/profile_default/startup
+ln -s ~/.spacemacs.d/linux-config/.gitconfig ~/.gitconfig 
+ln -s ~/.spacemacs.d/linux-config/bash_aliases ~/.bash_aliases 
+```
---
+++ linux-config/ipython_startup/gogo.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+
+try:
+    import morphio as mio
+except:
+    pass
+try:
+    import pandas as pd
+except:
+    pass
+try:
+    import numpy as np
+except:
+    pass
+try:
+    import neurom as nm
+except:
+    pass
+try:
+    import matplotlib.pyplot as plt
+    from matplotlib.pyplot import ion
+    ion()
+except:
+    pass
+
+try:
+    from morph_tool.morphdb import MorphDB
+
+    new = MorphDB.from_neurondb(Path('new/neuronDB.xml'), label='new')
+    old = MorphDB.from_neurondb(Path('old/neuronDB.xml'), label='old')
+    db = old + new
+    df = db.df
+except:
+    pass
